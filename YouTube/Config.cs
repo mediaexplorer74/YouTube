5d825a036996511d3da5efb3e3eba382b78b4cff
zemonkamin <95709073+zemonkamin@users.noreply.github.com>
2025-05-23 18:52:10 +0300

    Add files via upload

---
+++ YouTube/Config.cs
@@ -0,0 +1,21 @@
+﻿public static class Config
+{
+    private static string _apiBaseUrl = "https://qqq.bccst.ru/youtube/";
+    private static string _defaultQuality = "360";
+
+    public static string ApiBaseUrl
+    {
+        get { return _apiBaseUrl; }
+        set { _apiBaseUrl = value.EndsWith("/") ? value : value + "/"; }
+    }
+
+    public static string DefaultQuality
+    {
+        get { return _defaultQuality; }
+        set { _defaultQuality = value; }
+    }
+
+    public static int MaxCardsPerPage { get; } = 10;
+    public static int CardHeight { get; } = 400;
+    public static int ThumbnailHeight { get; } = 280;
+}
